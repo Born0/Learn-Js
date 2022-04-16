2221848cd3d8c747e85c3fb89134716cb70bf8d7
Born0 <bornoahmed2@gmail.com>
2022-04-17 02:31:50 +0600

    basic Js complete

--- dom.js
+++ dom.js
@@ -5,20 +5,28 @@
 //----------------- // HtmlCollection
 // console.log(document.getElementsByClassName('item'));
 
-const items= document.querySelectorAll('.item');
-console.log(items);
+// const items= document.querySelectorAll('.item');
+// console.log(items);
 
-items.forEach(i=> console.log(i));
+// items.forEach(i=> console.log(i));
 
-const ul=document.querySelector('.items');
+ const ul=document.querySelector('.items');
 
 //ul.lastElementChild.remove();
 //ul.firstElementChild.textContent='first li';
-ul.children[1].innerText='child 1';
-ul.lastElementChild.innerHTML='<h1>'
-                + ul.lastElementChild.textContent+'</h1>';
+// ul.children[1].innerText='child 1';
+// ul.lastElementChild.innerHTML='<h1>'
+//           + ul.lastElementChild.textContent+'</h1>';
 const btn=document.querySelector('.btn');
 
+//btn.addEventListener('mouseover',(e)=>{
 btn.addEventListener('click',(e)=>{
-    console.log('click');
+    e.preventDefault();
+    //console.log(e);
+    //console.log(e.target.className);
+    const myForm= document.querySelector('#my-form');
+    myForm.style.background='#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+
+    //ul.lastElementChild.innerHTML="<h1> Hello </h1>"
 });
